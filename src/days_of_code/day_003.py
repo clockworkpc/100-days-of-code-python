6179fe8e6bafafb368a2f899d0d2a21b4503e69c
Alexander Garber <clockworkpc@gmail.com>
2024-04-13 08:34:53 +0300

    enhanced pytesting

--- src/days_of_code/day_003.py
+++ src/days_of_code/day_003.py
@@ -1,56 +1,56 @@
 """Day 3: Treasure Island"""
 
 import os
-
-assets_path = os.path.join(os.getcwd(), "src", "days_of_code", "assets")
+import json
 
 
 class Game:
 
     def __init__(self):
         self.messages = []
+        self.assets_path = os.path.join(os.getcwd(), "src", "days_of_code", "assets")
+        json_path = os.path.join(self.assets_path, "day_003_scenarios.json")
+        self.scenes = json.load(open(json_path, "r"))
+        ascii_path = os.path.join(self.assets_path, "ascii_treasure.txt")
+        self.ascii_banner = open(ascii_path, "r").read()
 
-    def print_banner_ascii(self):
-        file_path = os.path.join(assets_path, "ascii_treasure.txt")
-        with open(file_path, "r") as file:
-            file_contents = file.read()
-            print(file_contents)
+    def play(self):
+        self.print_game_intro()
 
+        if self.scenario(self.scenes["s1"]) is False:
+            return self.game_over()
+
+        if self.scenario(self.scenes["s2"]) is False:
+            return self.game_over()
+
+        return self.game_over(self.scenario(self.scenes["s3"], False))
+
+    def print_game_intro(self):
+        print(self.ascii_banner)
         print("Welcome to Treasure Island.")
-        print("Your mission to find the treasure.")
+        print("Your mission is to find the treasure.\n")
 
-    def scenario(self, intro, choose, choices, default=True):
-        print(intro + " ")
-        choice = input(choose + " ").strip()
-        print(f"CHOICE: {choice}")
+    def scenario(self, hsh, default=False):
+        description, question, choices, default_message = (
+            hsh["description"],
+            hsh["question"],
+            hsh["choices"],
+            hsh["default_message"],
+        )
+
+        print(description + " ")
+        response = input(question + " ").strip()
+        print(f"Your choice: {response}")
+
         for my_dict in choices:
-            if choice.lower() == my_dict["choice"]:
+            if response.lower() == my_dict["choice"]:
+                # To avoid cluttering self.messages with None elements
                 if my_dict["message"] is not None:
                     self.messages.append(my_dict["message"])
                 return my_dict["bool"]
+
+        self.messages.append(default_message)
         return default
-
-    def scenario1(self):
-        intro = "You reach a fork in the road."
-        choose = "left or right?"
-        choices = [{"choice": "right", "message": "Fall into a hole.", "bool": False}]
-        return self.scenario(intro, choose, choices)
-
-    def scenario2(self):
-        intro = "You reach a river and see a ferry in the distance."
-        choose = "swim or wait?"
-        choices = [{"choice": "swim", "message": "Attacked by trout.", "bool": False}]
-        return self.scenario(intro, choose, choices)
-
-    def scenario3(self):
-        intro = "You arrive at three doors."
-        choose = "Which door?  Red, Blue, Yellow? "
-        choices = [
-            {"choice": "yellow", "message": None, "bool": True},
-            {"choice": "red", "message": "Burned by fire.", "bool": False},
-            {"choice": "blue", "message": "Eaten by beasts.", "bool": False},
-        ]
-        return self.scenario(intro, choose, choices, False)
 
     def game_over(self, bool=False):
         if bool is False:
@@ -61,17 +61,6 @@ class Game:
         joined_messages = "\n".join(self.messages)
         print(joined_messages)
         return joined_messages
-
-    def play(self):
-        self.print_banner_ascii()
-
-        if self.scenario1() is False:
-            return self.game_over()
-
-        if self.scenario2() is False:
-            return self.game_over()
-
-        return self.game_over(self.scenario3())
 
 
 def main():
--- tests/test_day_003.py
+++ tests/test_day_003.py
@@ -3,51 +3,138 @@
 """Tests for `day_003` package."""
 
 from src.days_of_code import day_003 as d
+import pytest
 
 
-def test_scenario_1a(monkeypatch):
-    inputs = iter(["right"])
-    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+@pytest.fixture
+def scenes():
     game = d.Game()
-    result = game.play()
-    assert result == "Fall into a hole.\nGame over."
+    yield game.scenes
 
 
-def test_scenario_2a(monkeypatch):
-    inputs = iter(["left", "swim"])
+def test_json(scenes):
+    assert scenes["s1"]["choices"][0]["choice"] == "left"
+
+
+# def test_scenario_1a1(monkeypatch):
+#     inputs = iter(["right"])
+#     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+#     game = d.Game()
+#     result = game.play()
+#     assert result == "Fall into a hole.\nGame over."
+
+
+def individual_scenario_test(scenes, scene_key, input_list, expectation):
+    monkeypatch = pytest.MonkeyPatch()
+    inputs = iter(input_list)
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    scene = scenes[scene_key]
     game = d.Game()
-    result = game.play()
-    assert result == "Attacked by trout.\nGame over."
+    result = game.scenario(scene)
+    assert result is expectation
 
 
-def test_scenario_3a(monkeypatch):
-    inputs = iter(["left", "wait", "red"])
+def test_scenario1(scenes):
+    individual_scenario_test(scenes, "s1", ["left"], True)
+    individual_scenario_test(scenes, "s1", ["right"], False)
+    individual_scenario_test(scenes, "s1", ["foo"], False)
+
+
+def test_scenario2(scenes):
+    individual_scenario_test(scenes, "s2", ["wait"], True)
+    individual_scenario_test(scenes, "s2", ["swim"], False)
+    individual_scenario_test(scenes, "s2", ["foo"], False)
+
+
+def test_scenario3(scenes):
+    individual_scenario_test(scenes, "s3", ["yellow"], True)
+    individual_scenario_test(scenes, "s3", ["red"], False)
+    individual_scenario_test(scenes, "s3", ["blue"], False)
+    individual_scenario_test(scenes, "s3", ["foo"], False)
+
+
+# def test_scenario_1a(scenes):
+#     individual_scenario_test(scenes, "s1", ["left"], True)
+#
+#
+# def test_scenario_1b(monkeypatch, scenes):
+#     individual_scenario_test(scenes, "s1", ["right"], False)
+#
+#
+# def test_scenario_1c(monkeypatch, scenes):
+#     individual_scenario_test(scenes, "s1", ["foo"], False)
+
+
+def test_scenario_2a(monkeypatch, scenes):
+    inputs = iter(["wait"])
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    scene = scenes["s2"]
     game = d.Game()
-    result = game.play()
-    assert result == "Burned by fire.\nGame over."
+    result = game.scenario(scene)
+    assert result is True
 
 
-def test_scenario_3b(monkeypatch):
-    inputs = iter(["left", "wait", "blue"])
+def test_scenario_2b(monkeypatch, scenes):
+    inputs = iter(["swim"])
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    scene = scenes["s2"]
     game = d.Game()
-    result = game.play()
-    assert result == "Eaten by beasts.\nGame over."
+    result = game.scenario(scene)
+    assert result is False
 
 
-def test_scenario_3c(monkeypatch):
-    inputs = iter(["left", "wait", "foo"])
+def test_scenario_2c(monkeypatch, scenes):
+    inputs = iter(["foo"])
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    scene = scenes["s2"]
     game = d.Game()
-    result = game.play()
-    assert result == "Game over."
+    result = game.scenario(scene)
+    assert result is False
 
 
-def test_scenario_3d(monkeypatch):
-    inputs = iter(["left", "wait", "yellow"])
-    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
-    game = d.Game()
-    result = game.play()
-    assert result == "You win!"
+# def test_scenario_2a1(monkeypatch):
+#     inputs = iter(["left", "swim"])
+#     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+#     game = d.Game()
+#     result = game.play()
+#     assert result == "Attacked by trout.\nGame over."
+
+
+# def test_scenario_2a2(monkeypatch):
+#     inputs = iter(["left", "foo"])
+#     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+#     game = d.Game()
+#     result = game.play()
+#     assert result == "Attacked by trout.\nGame over."
+
+
+# def test_scenario_3a(monkeypatch):
+#     inputs = iter(["left", "wait", "red"])
+#     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+#     game = d.Game()
+#     result = game.play()
+#     assert result == "Burned by fire.\nGame over."
+#
+#
+# def test_scenario_3b(monkeypatch):
+#     inputs = iter(["left", "wait", "blue"])
+#     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+#     game = d.Game()
+#     result = game.play()
+#     assert result == "Eaten by beasts.\nGame over."
+#
+#
+# def test_scenario_3c(monkeypatch):
+#     inputs = iter(["left", "wait", "foo"])
+#     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+#     game = d.Game()
+#     result = game.play()
+#     assert result == "Game over."
+#
+#
+# def test_scenario_3d(monkeypatch):
+#     inputs = iter(["left", "wait", "yellow"])
+#     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+#     game = d.Game()
+#     result = game.play()
+#     assert result == "You win!"
