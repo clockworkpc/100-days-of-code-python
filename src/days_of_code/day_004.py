7e5a574c3ac982950fdceaf24f41e1438a8fca51
Alexander Garber <clockworkpc@gmail.com>
2024-04-14 21:29:51 +0300

    refining day 04

--- src/days_of_code/day_004.py
+++ src/days_of_code/day_004.py
@@ -1,21 +1,21 @@
 """Day 4: Rock Paper Scissors"""
 
 import os
+import sys
 import json
 import random
 
 
 class Game:
 
-    def __init__(self):
-        self.ascii_path = os.path.join(
-            os.getcwd(), "src", "days_of_code", "assets", "ascii"
-        )
-        json_path = os.path.join(
-            os.getcwd(), "src", "days_of_code", "assets", "json", "day_004_rules.json"
-        )
-
+    def __init__(self, rounds=5):
+        assets_ary = "src/days_of_code/assets".split("/")
+        self.ascii_path = os.path.join(os.getcwd(), *assets_ary, "ascii")
+        json_ary = assets_ary + "json/day_004_rules.json".split("/")
+        json_path = os.path.join(os.getcwd(), *json_ary)
         self.rules = json.load(open(json_path, "r"))
+        self.score = {"user": 0, "pc": 0}
+        self.rounds = rounds
 
     def play_round(self, hand1, hand2):
         return self.rules[hand1]["results"][hand2]
@@ -27,54 +27,156 @@ class Game:
         return None
 
     def instructions(self):
-        options = []
-        for k, v in self.rules.items():
-            options.append(f"{str( v['code'] )} for { k.capitalize() }")
-
-        options = ", ".join(options)
-        return " ".join(["What do you choose? Type", options, ": "])
+        options_ary = [
+            f"{v['code']} for {k.capitalize()}" for k, v in self.rules.items()
+        ] + ["Q for Quit"]
+        return " ".join(["Type", ", ".join(options_ary), "\nYour choice: "])
 
     def print_hands(self, user_hand, pc_hand):
         user_ascii_path = os.path.join(self.ascii_path, f"{user_hand}.txt")
-        user_ascii = ascii_img = open(user_ascii_path, "r").read()
+        user_ascii = open(user_ascii_path, "r").read()
         pc_ascii_path = os.path.join(self.ascii_path, f"{pc_hand}.txt")
-        pc_ascii = ascii_img = open(pc_ascii_path, "r").read()
-        print("You chose:")
+        pc_ascii = open(pc_ascii_path, "r").read()
+        print(f"You chose: {user_hand}")
         print(user_ascii)
-        print("Computer chose:")
+        print(f"Computer chose: {pc_hand}")
         print(pc_ascii)
 
     def print_outcome(self, result):
         if result == "win":
             print("You win!")
-        elif result == "draw":
+        elif result == "loss":
             print("You lose!")
         else:
             print("It's a draw!")
 
-    def play(self, preset_hand=None):
-        os.system("clear")
+    def update_score(self, result):
+        if result == "win":
+            self.score["user"] += 1
+        elif result == "loss":
+            self.score["pc"] += 1
+
+    def print_score(self):
+        print(f"You: {self.score['user']} || PC: {self.score['pc']}")
+
+    def prepare_hands(self, preset_hand):
+
         user_choice = input(self.instructions())
+        if user_choice.lower() == "q":
+            return ["quit", None]
+
         user_hand = self.find_key_by_code(int(user_choice))
 
         if user_hand is None:
+            print(f"USER HAND: {user_choice} --> {user_hand}")
             print("This option does not exist.")
-            return
+            return [None, None]
 
         if preset_hand is None:
             random_hand = random.choice(list(self.rules.keys()))
 
         pc_hand = preset_hand or random_hand
-        result = self.play_round(user_hand, pc_hand)
 
+        return [user_hand, pc_hand]
+
+    def update_screen(self, result, user_hand, pc_hand):
+        self.update_score(result)
         self.print_hands(user_hand, pc_hand)
         self.print_outcome(result)
+        self.print_score()
 
-        return result
+    def declare_winner(self):
+        msg = {
+            self.score["user"] > self.score["pc"]: "You won the game!",
+            self.score["user"] < self.score["pc"]: "The computer won the game!",
+        }.get(True, "It's a draw!")
+
+        print(msg)
+
+    def initial_game_values(self):
+        current_result = None
+        counter = 0
+        running = True
+        return [current_result, counter, running]
+
+    def start_screen(self):
+        os.system("clear")
+        self.print_score()
+        print(f"ROUNDS: { self.rounds}")
+
+    def play_single_round(
+        self, preset_hand, current_result, counter, running, single_round
+    ):
+        user_hand, pc_hand = self.prepare_hands(preset_hand)
+        os.system("clear")
+        #
+        if user_hand is None and single_round:
+            print("This option does not exist.  Thanks for playing!")
+            return
+
+        if user_hand is None:
+            print("This option does not exist.")
+            return
+
+        if user_hand == "quit":
+            print("Thanks for playing!")
+            return current_result
+
+        result = self.play_round(user_hand, pc_hand)
+        current_result = result
+
+        self.update_screen(result, user_hand, pc_hand)
+
+        counter += 1
+        print(f"COUNTER: {counter}, ROUNDS: {self.rounds}")
+        if counter == self.rounds or single_round:
+            self.declare_winner()
+            print("Thanks for playing!")
+            return current_result
+
+    def play(self, preset_hand=None, single_round=False):
+
+        self.start_screen()
+        current_result, counter, running = self.initial_game_values()
+
+        while running:
+            self.play_single_round(preset_hand, single_round, *self.initial_game_values())
+            # user_hand, pc_hand = self.prepare_hands(preset_hand)
+            # os.system("clear")
+            # #
+            # if user_hand is None and single_round:
+            #     print("This option does not exist.  Thanks for playing!")
+            #     return
+            #
+            # if user_hand is None:
+            #     print("This option does not exist.")
+            #     continue
+            #
+            # if user_hand == "quit":
+            #     print("Thanks for playing!")
+            #     return current_result
+            #
+            # result = self.play_round(user_hand, pc_hand)
+            # current_result = result
+            #
+            # self.update_screen(result, user_hand, pc_hand)
+            #
+            # counter += 1
+            # print(f"COUNTER: {counter}, ROUNDS: {self.rounds}")
+            # if counter == self.rounds or single_round:
+            #     self.declare_winner()
+            #     print("Thanks for playing!")
+            #     return current_result
+
+        return current_result
 
 
 def main():
-    new_game = Game()
+    if len(sys.argv) > 1 and isinstance(int(sys.argv[1]), int):
+        new_game = Game(int(sys.argv[1]))
+    else:
+        new_game = Game()
+
     new_game.play()
 
 
--- tests/test_day_004.py
+++ tests/test_day_004.py
@@ -5,28 +5,10 @@
 from src.days_of_code import day_004 as d
 import pytest
 
-# open(ascii_path, "r").read()
-
-# rps_folder_path = os.path.join(os.getcwd(), "src", "days_of_code", "assets", "ascii")
-# rock_ascii_path = os.path.join(rps_folder_path, "rock.txt")
-# paper_ascii_path = os.path.join(rps_folder_path, "paper.txt")
-# scissors_ascii_path = os.path.join(rps_folder_path, "scissors.txt")
-#
-# rock_ascii = open(rock_ascii_path, "r").read()
-# paper_ascii = open(paper_ascii_path, "r").read()
-# scissors_ascii = open(scissors_ascii_path, "r").read()
-#
-
 
 @pytest.fixture
 def game():
     yield d.Game()
-
-
-# def test_ascii(game):
-#     assert game.ascii["rock"] == rock_ascii
-#     assert game.ascii["paper"] == paper_ascii
-#     assert game.ascii["scissors"] == scissors_ascii
 
 
 def test_rules_keys(game):
@@ -88,19 +70,11 @@ def test_play_round_scissors(game):
     assert result == "draw"
 
 
-# def test_play_rock(game):
-#     monkeypatch = pytest.MonkeyPatch()
-#     inputs = iter(["rock"])
-#     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
-#     result = game.play("rock")
-#     assert result == "draw"
-
-
 def play_test(game, hand1, hand2, expectation):
     monkeypatch = pytest.MonkeyPatch()
     inputs = iter([hand1])
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
-    result = game.play(hand2)
+    result = game.play(hand2, True)
     assert result == expectation
 
 
@@ -122,79 +96,5 @@ def test_play_scissors(game):
     play_test(game, "2", "scissors", "draw")
 
 
-# @pytest.fixture
-# def scenes():
-#     game = d.Game()
-#     yield game.scenes
-#
-#
-# def test_json(scenes):
-#     assert scenes["s1"]["choices"][0]["choice"] == "left"
-#
-#
-# def individual_scenario_test(scenes, scene_key, input_list, expectation):
-#     monkeypatch = pytest.MonkeyPatch()
-#     inputs = iter(input_list)
-#     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
-#     scene = scenes[scene_key]
-#     game = d.Game()
-#     result = game.scenario(scene)
-#     assert result is expectation
-#
-#
-# def scenario_sequence_test(scenes, hsh, expectation):
-#     monkeypatch = pytest.MonkeyPatch()
-#     input_list = list(hsh.values())
-#     inputs = iter(input_list)
-#     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
-#     game = d.Game()
-#     result = game.play()
-#     assert result == expectation
-#
-#
-# def test_scenario1(scenes):
-#     individual_scenario_test(scenes, "s1", ["left"], True)
-#     individual_scenario_test(scenes, "s1", ["right"], False)
-#     individual_scenario_test(scenes, "s1", ["foo"], False)
-#
-#
-# def test_sequence1(scenes):
-#     message = "Fall into a hole.\nGame over."
-#     scenario_sequence_test(scenes, {"s1": "right"}, message)
-#     scenario_sequence_test(scenes, {"s1": "foo"}, message)
-#
-#
-# def test_scenario2(scenes):
-#     individual_scenario_test(scenes, "s2", ["wait"], True)
-#     individual_scenario_test(scenes, "s2", ["swim"], False)
-#     individual_scenario_test(scenes, "s2", ["foo"], False)
-#
-#
-# def test_sequence2(scenes):
-#     message = "Attacked by trout.\nGame over."
-#     scenario_sequence_test(scenes, {"s1": "left", "s2": "swim"}, message)
-#     scenario_sequence_test(scenes, {"s1": "left", "s2": "foo"}, message)
-#
-#
-# def test_scenario3(scenes):
-#     individual_scenario_test(scenes, "s3", ["yellow"], True)
-#     individual_scenario_test(scenes, "s3", ["red"], False)
-#     individual_scenario_test(scenes, "s3", ["blue"], False)
-#     individual_scenario_test(scenes, "s3", ["foo"], False)
-#
-#
-# def test_sequence3(scenes):
-#     sequence3a = {"s1": "left", "s2": "wait", "s3": "red"}
-#     message1 = "Burned by fire.\nGame over."
-#     scenario_sequence_test(scenes, sequence3a, message1)
-#
-#     sequence3b = {"s1": "left", "s2": "wait", "s3": "blue"}
-#     message2 = "Eaten by beasts.\nGame over."
-#     scenario_sequence_test(scenes, sequence3b, message2)
-#
-#     sequence3c = {"s1": "left", "s2": "wait", "s3": "yellow"}
-#     message3 = "You win!"
-#     scenario_sequence_test(scenes, sequence3c, message3)
-#
-#     message4 = "Game over."
-#     scenario_sequence_test(scenes, {"s1": "left", "s2": "wait", "s3": "foo"}, message4)
+def test_play_error(game):
+    play_test(game, "3", "rock", None)
